255ee00b32ba61f1fa338cb88ddb2846ec9e074a
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-07-27 21:45:21 +0300

    Implement human and computer choice logic

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
+function getComputerChoice() {
+  let choice = Math.floor(Math.random() * 3);
 
+  switch (choice) {
+    case 0:
+      return "rock";
+    case 1:
+      return "paper";
+    case 2:
+      return "scissors";
+    default:
+      return null;
+  }
+}
+
+function getHumanChoice() {
+  return prompt(
+    "Rock-Paper-Scissors! Your Move! (enter 'Rock', 'Paper' or 'Scissors')"
+  );
+}
