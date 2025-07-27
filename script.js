480e77b3db9d2fe401f73e91f4fefb1b1877b477
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-07-27 21:46:09 +0300

    Implement the logic of a single round

--- script.js
+++ script.js
@@ -18,3 +18,40 @@ function getHumanChoice() {
     "Rock-Paper-Scissors! Your Move! (enter 'Rock', 'Paper' or 'Scissors')"
   );
 }
+
+let humanScore = 0;
+let computerScore = 0;
+
+function humanWin() {
+  console.log("Victory! You've beaten the computer!");
+  humanScore++;
+}
+
+function computerWin() {
+  console.log("Oops... You lost!");
+  computerScore++;
+}
+
+function playRound(humanChoice, computerChoice) {
+  let humanChoiceLC = humanChoice.toLowerCase();
+  // Computer choice is already lowercased in getComputerChoice function
+
+  if (humanChoiceLC === computerChoice) {
+    console.log("It's a tie!");
+  }
+
+  switch (humanChoiceLC) {
+    case "rock":
+      // The computer choosing rock is covered in a tie section, so ternary operator is used
+      computerChoice === "scissors" ? humanWin() : computerWin();
+      break;
+    case "paper":
+      computerChoice === "rock" ? humanWin() : computerWin();
+      break;
+    case "scissors":
+      computerChoice === "paper" ? humanWin() : computerWin();
+      break;
+    default:
+      console.log("Hm... your choice wasn't recognized.");
+  }
+}
