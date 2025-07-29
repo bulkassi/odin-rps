d10601dcdfec50d45a6792aafc414c8e9eb94fe5
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-07-29 11:58:35 +0300

    Add results display for current scores and round message
    The playGame function is returned to incapsulate the game properties

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let playButtons = document.querySelector("#play-buttons");
+let results = document.querySelector("#results");
 
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
@@ -15,49 +16,71 @@ function getComputerChoice() {
   }
 }
 
-playButtons.addEventListener("click", (e) => {
-  let targetButton = e.target;
+function playGame() {
+  playButtons.addEventListener("click", (e) => {
+    let targetButton = e.target;
 
-  if (targetButton.parentNode.id === "play-buttons") {
-    // Since all play buttons, and only them, are children of play-buttons div
-    playRound(targetButton.id, getComputerChoice());
-  }
-});
+    if (targetButton.parentNode.id === "play-buttons") {
+      // Since all play buttons, and only them, are children of play-buttons div
+      playRound(targetButton.id, getComputerChoice());
+    }
+  });
 
-let humanScore = 0;
-let computerScore = 0;
+  let humanScore = 0;
+  let computerScore = 0;
 
-function humanWin() {
-  console.log("Victory! You've beaten the computer!");
-  humanScore++;
-}
-
-function computerWin() {
-  console.log("Oops... You lost!");
-  computerScore++;
-}
-
-function playRound(humanChoice, computerChoice) {
-  let humanChoiceLC = humanChoice.toLowerCase();
-  // Computer choice is already lowercased in getComputerChoice function
-
-  if (humanChoiceLC === computerChoice) {
-    console.log("It's a tie!");
-    return;
+  function updateScoreText() {
+    for (child of results.children) {
+      switch (child.id) {
+        case "human-score":
+          child.textContent = "Your score - " + humanScore;
+          break;
+        case "computer-score":
+          child.textContent = "Computer score - " + computerScore;
+          break;
+      }
+    }
   }
 
-  switch (humanChoiceLC) {
-    case "rock":
-      // The computer choosing rock is covered in a tie section, so ternary operator is used
-      computerChoice === "scissors" ? humanWin() : computerWin();
-      break;
-    case "paper":
-      computerChoice === "rock" ? humanWin() : computerWin();
-      break;
-    case "scissors":
-      computerChoice === "paper" ? humanWin() : computerWin();
-      break;
-    default:
-      console.log("Hm... your choice wasn't recognized.");
+  function humanWin() {
+    humanScore++;
+    updateScoreText();
+
+    results.querySelector("#message").textContent =
+      "Victory! You've beaten the computer!";
+  }
+
+  function computerWin() {
+    computerScore++;
+    updateScoreText();
+
+    results.querySelector("#message").textContent = "Oops... You lost!";
+  }
+
+  function playRound(humanChoice, computerChoice) {
+    let humanChoiceLC = humanChoice.toLowerCase();
+    // Computer choice is already lowercased in getComputerChoice function
+
+    if (humanChoiceLC === computerChoice) {
+      results.querySelector("#message").textContent = "It's a tie!";
+      return;
+    }
+
+    switch (humanChoiceLC) {
+      case "rock":
+        // The computer choosing rock is covered in a tie section, so ternary operator is used
+        computerChoice === "scissors" ? humanWin() : computerWin();
+        break;
+      case "paper":
+        computerChoice === "rock" ? humanWin() : computerWin();
+        break;
+      case "scissors":
+        computerChoice === "paper" ? humanWin() : computerWin();
+        break;
+      default:
+        console.log("Hm... your choice wasn't recognized.");
+    }
   }
 }
+
+playGame();
