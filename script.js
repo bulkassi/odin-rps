b5c3bfb68bb0237b02a27a8ccfe97a95738ee5e7
bulkassi <158156178+bulkassi@users.noreply.github.com>
2025-07-29 11:38:32 +0300

    Add three buttons for play moves
    Using the DOM methods for handling the round plays

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+let playButtons = document.querySelector("#play-buttons");
+
 function getComputerChoice() {
   let choice = Math.floor(Math.random() * 3);
 
@@ -13,57 +15,49 @@ function getComputerChoice() {
   }
 }
 
-function getHumanChoice() {
-  return prompt(
-    "Rock-Paper-Scissors! Your Move! (enter 'Rock', 'Paper' or 'Scissors')"
-  );
+playButtons.addEventListener("click", (e) => {
+  let targetButton = e.target;
+
+  if (targetButton.parentNode.id === "play-buttons") {
+    // Since all play buttons, and only them, are children of play-buttons div
+    playRound(targetButton.id, getComputerChoice());
+  }
+});
+
+let humanScore = 0;
+let computerScore = 0;
+
+function humanWin() {
+  console.log("Victory! You've beaten the computer!");
+  humanScore++;
 }
 
-function playGame() {
-  let humanScore = 0;
-  let computerScore = 0;
-
-  function humanWin() {
-    console.log("Victory! You've beaten the computer!");
-    humanScore++;
-  }
-
-  function computerWin() {
-    console.log("Oops... You lost!");
-    computerScore++;
-  }
-
-  function playRound(humanChoice, computerChoice) {
-    let humanChoiceLC = humanChoice.toLowerCase();
-    // Computer choice is already lowercased in getComputerChoice function
-
-    if (humanChoiceLC === computerChoice) {
-      console.log("It's a tie!");
-      return;
-    }
-
-    switch (humanChoiceLC) {
-      case "rock":
-        // The computer choosing rock is covered in a tie section, so ternary operator is used
-        computerChoice === "scissors" ? humanWin() : computerWin();
-        break;
-      case "paper":
-        computerChoice === "rock" ? humanWin() : computerWin();
-        break;
-      case "scissors":
-        computerChoice === "paper" ? humanWin() : computerWin();
-        break;
-      default:
-        console.log("Hm... your choice wasn't recognized.");
-    }
-  }
-
-  console.log(
-    "End of the game! Total score:\nYOU: " +
-      humanScore +
-      "\nCOMPUTER: " +
-      computerScore
-  );
+function computerWin() {
+  console.log("Oops... You lost!");
+  computerScore++;
 }
 
-playGame();
+function playRound(humanChoice, computerChoice) {
+  let humanChoiceLC = humanChoice.toLowerCase();
+  // Computer choice is already lowercased in getComputerChoice function
+
+  if (humanChoiceLC === computerChoice) {
+    console.log("It's a tie!");
+    return;
+  }
+
+  switch (humanChoiceLC) {
+    case "rock":
+      // The computer choosing rock is covered in a tie section, so ternary operator is used
+      computerChoice === "scissors" ? humanWin() : computerWin();
+      break;
+    case "paper":
+      computerChoice === "rock" ? humanWin() : computerWin();
+      break;
+    case "scissors":
+      computerChoice === "paper" ? humanWin() : computerWin();
+      break;
+    default:
+      console.log("Hm... your choice wasn't recognized.");
+  }
+}
